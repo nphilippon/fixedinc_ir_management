2a99434d3764663a28acc962cbd221d419ade17c
AymanArman <aaarman@ualberta.ca>
2026-02-19 14:38:34 -0700

    init builder page

--- ui.R
+++ ui.R
@@ -1,18 +1,29 @@
 library(shiny)
 
+
+
+
 dashboardPage(
   dashboardHeader(title = "IR App"),
-  
+  # Icon Options Here: https://fontawesome.com/  
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Raw Data", tabName = "data", icon = icon("table"))
+      menuItem("Raw Data", tabName = "data", icon = icon("table")),
+      menuItem("Portfolio Builder", tabName = "builder", icon = icon("wrench"))
     )
   ),
   dashboardBody(
+    tabItems(
       tabItem(tabName = "data",
               fluidRow(
                 box(width = 12, title = "Treasury Yield Data", status = "info",
                     DTOutput("yieldTable"))
+              )
+      ),
+      tabItem(tabName = "builder",
+              fluidPage(
+                titlePanel("Build Your Portfolio"))
+              
               )
       ))
   )
